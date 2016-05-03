8fd0039a981a9ab71019ee8a0f2e34eb5f96e3b6
HungNguyen <nguyenphanmanhhung@gmail.com>
2016-05-03 22:19:46 +0700

    Update prob 1 - tested, prob2 - not tested

--- FinalProject/Project.py
+++ FinalProject/Project.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import numpy as np
 from collections import deque
 
 def readDFAtable(f):
@@ -44,12 +45,11 @@ def getClosure(EpsDfaTable, start, label):
 	while qsize > 0:
 		u = q.pop();
 		n = n - 1;
-		if (start, label) in EpsDfaTable:
-			for v in EpsDfaTable[start, label]:
-				if v not in ret:
-					q.append(v);
-					ret.append(v);
-					n = n + 1;
+		for v in EpsDfaTable[start, label]:
+			if v not in ret:
+				q.append(v);
+				ret.append(v);
+				n = n + 1;
 
 	return ret;
 
@@ -62,8 +62,7 @@ def readEpsDFAtable(f):
 	for i in range(n):
 		for j in range(m):
 			line = map(int, f.readline().split());
-			if line[0] != 0:
-				EpsDfaTable[i, alphabet[j]] = [];
+			EpsDfaTable[i, alphabet[j]] = [];
 			for k in range(1, line[0]+1):
 				EpsDfaTable[i, alphabet[j]].append(line[k]);
 
@@ -72,12 +71,67 @@ def readEpsDFAtable(f):
 def prob2():
 	f = open('input2.txt', 'r');
 	EpsDfaTable, alphabet, acceptedState = readEpsDFAtable(f);
+	f.close();
 
+	start = getClosure(0);
+	id = dict();
+	id[start] = 0;
+	dfaState = [start];
+	maxId = 1;
+
+	dfaTable = dict();
 	q = deque();
-	q.append(getClosure(0));
+	q.append(state[0]);
 	n = 1;
+	while n > 0:
+		u = q.pop();
+		n = n - 1;
 
-	
+		for label in alphabet:
+			v = [];
+			temp = []
+			for ne in u:
+				temp.extend(EpsDfaTable[ne, label]);
+			temp = np.unique(temp).tolist();
+
+			for x in temp:
+				temp1 = getClosure(EpsDfaTable, x);
+				v.extend(temp1);
+
+			v = np.unique(v).tolist();
+
+			if (v not in id.keys()):
+				n = n + 1;
+				q.append(v);
+				
+				dfaState.append(v);
+
+				id[v] = maxId;
+				maxId += 1;
+			
+			dfaTable[id[u], label] = id[v];
+
+	dfaAcceptedState = [];
+	for u in dfaState:
+		for fin in acceptedState:
+			if fin in u:
+				dfaAcceptedState.append(id[u]);
+				break;
+
+	g = open('output2.txt', 'w');
+	g.write(str(len(dfaState)) + ' ' + str(len(alphabet)) + ' ' + str(len(dfaAcceptedState)) + '\n');
+	g.write(' '.join(map(str, alphabet)) + '\n');
+	g.write(' '.join(map(str, dfaAcceptedState)) + '\n');
+
+	for u in dfaState:
+		li = [];
+		for label in alphabet:
+			li.append(dfaTable[u, label]);
+		g.write(' '.join(map(str, li)) + '\n');
+
+	for st in dfaState:
+		g.write(str(id[st]) + ': ' + str(st) + '\n');
+	g.close();
 
 """A multi-producer, multi-consumer queue."""
 
