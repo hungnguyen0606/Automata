40d28f079af1bba45bee7d2eeef91691c8c2a367
HungNguyen <nguyenphanmanhhung@gmail.com>
2016-05-06 23:45:12 +0700

    Update: prob4 - temp

--- FinalProject/Project.py
+++ FinalProject/Project.py
@@ -167,14 +167,14 @@ def prob2():
 	for st in dfaState:
 		g.write(str(id[frozenset(st)]) + ': ' + str(st) + '\n');
 	g.close();
-
+#------------------------------------------------------------------------------------------
 def prob5():
 	#input
 	print 'Start to solve 5:';
 	f = open('input5.txt', 'r');
 	allState, alphabet, startingState, dfaTable, acceptedState = readDFAtable(f);
 	f.close();
-
+	#--------------------------------------------------------------------
 	#init
 	newAllState = [];
 	newAlphabet = [];
@@ -203,6 +203,7 @@ def prob5():
 			for v in acceptedState:
 				tabFil[u, v] = tabFil[v, u] = 1;
 	#---------------------------
+	#filling table
 	while True:
 		ok = False;
 		for u in allState:
@@ -216,6 +217,7 @@ def prob5():
 		if (ok == False):
 			break;
 
+	#grouping states that can't be distinguished
 	id = dict();
 	maxID = 0;
 	for i in range(len(allState)):
@@ -225,6 +227,8 @@ def prob5():
 			for j in range(i+1, len(allState)):
 				if tabFil[allState[i], allState[j]] == 0:
 					id[allState[j]] = id[allState[i]];
+	#------------------------------------------------
+	#create new dfa
 	for u in allState:
 		for label in alphabet:
 			newDfaTable[id[u], label] = id[dfaTable[u, label]];
@@ -234,13 +238,106 @@ def prob5():
 	newAcceptedState = np.unique([id[u] for u in acceptedState]).tolist();
 	newAllState = np.unique([id[u] for u in allState]).tolist();
 	#newAcceptedState = [u for u in newAcceptedState]
-	g = open('output5.txt', 'w');
-	# allState, alphabet, startingState, dfaTable, acceptedState):
-	writeDFAtable(g, newAllState, newAlphabet, newStartingState, newDfaTable, newAcceptedState);
 
+	#print output
+	g = open('output5.txt', 'w');
+	writeDFAtable(g, newAllState, newAlphabet, newStartingState, newDfaTable, newAcceptedState);
 	g.write('Description\n');
 	for u in newAllState:
 		g.write(str(u) + ': ' + ' '.join(map(str, [v for v in allState if id[v] == u])) + '\n')
 	g.close();
 
-prob5();
+
+#-------------------------------------------------------------------------
+def pre1(u):
+
+	if u == 'eps' or u == '':
+		return u;
+	return '(' + u + ')';
+
+def pre2(u):
+	if u == 'eps':
+		return '';
+	return '(' + u + ')';
+def concat(u, v):
+	if u == '' or v == '':
+		return '';
+	if u == 'eps':
+		return v;
+	if v == 'eps':
+		return u;
+	return '(' + u + ')' + '(' + v + ')';
+
+def prob4():
+	#def readDFAtable(f):
+	#return (allState,  alphabet, startingState, dfaTable, acceptedState)
+	#input
+	f = open('input4.txt', 'r');
+	allState,  alphabet, startingState, dfaTable, acceptedState = readDFAtable(f);
+	f.close();
+	#-------------------------------------------------------------------
+	#convert to graph
+	adj = dict();
+	for u in allState:
+		for label in alphabet:
+			v = dfaTable[u, label];
+			if (u, v) in adj.keys():
+				adj[u, v] = adj[u, v] + '+' + str(label);
+			else:
+				adj[u, v] = str(label);
+
+	for u in allState:
+		for v in allState:
+			if (u, v) not in adj.keys():
+				adj[u, v] = '';
+
+	newStartingState = 'new Starting';
+	newEndingState = 'new Ending';
+
+	for u in allState:
+		adj[newStartingState, u] = '';
+		adj[u, newEndingState] = '';
+
+	for u in acceptedState:
+		adj[u, newEndingState] = 'eps';
+
+	adj[newStartingState, startingState] = 'eps';
+	adj[newStartingState, newEndingState] = ''
+	#-------------------------------------------------------------------
+	#removing node
+	allState.append(newStartingState);
+	allState.append(newEndingState);
+
+	for i in range(len(allState) - 2):
+		mid = allState[i];
+
+		for j in range(i+1, len(allState) - 1):
+			u = allState[j];
+			for k in range(i+1, len(allState)):
+				v = allState[k];
+				if (u == '5' and v == '6'):
+					x = 12;
+				if v != newStartingState:
+					tt = '';
+					if adj[mid, mid] == '' or adj[mid, mid] == 'eps':
+						tt = 'eps';
+					else:
+						tt = '(' + adj[mid, mid] + ')*';
+					temp = concat(concat(adj[u, mid], tt), adj[mid, v]);
+
+
+					if adj[u, v] == 'eps' and temp == 'eps':
+						adj[u, v] = 'eps';
+					elif adj[u, v] == '' and temp == '':
+						adj[u, v] = '';
+					elif adj[u, v] == '' or temp == '':
+						adj[u, v] = adj[u, v] + temp;
+					else:
+						adj[u, v] = pre1(adj[u, v]) + '+' + pre1(temp);
+					#adj[u, v] = adj[u, v] + '+' + temp
+
+	g = open('output4.txt', 'w');
+	g.write(adj[newStartingState, newEndingState]);
+	g.close();
+
+prob4();
